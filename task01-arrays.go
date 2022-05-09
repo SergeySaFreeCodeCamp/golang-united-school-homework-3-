70e0d5c2fdb4d4fe1678cc1903057de3773447e6
SergeySA <aledej@ya.ru>
2022-05-09 19:38:05 +0600

    lecture-03-solution

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,16 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
+	result = float32(0)
+
+	for indx1, indx2 := 0, len(input)-1; indx1 < indx2; {
+		result += input[indx1] + input[indx2]
+		indx1++
+		indx2--
+		if indx1 == indx2 {
+			result += input[indx1]
+		}
+	}
+	result = result / float32(len(input))
 	return
 }
--- task02-slice.go
+++ task02-slice.go
@@ -2,5 +2,16 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
+	result = append(result, input...)
+
+	for indx1, indx2 := 0, len(input)-1; indx1 < indx2; {
+		result[indx1], result[indx2] = input[indx2], input[indx1]
+		indx1++
+		indx2--
+		if indx1 == indx2 {
+			result[indx1] = input[indx1]
+		}
+	}
+
 	return
 }
